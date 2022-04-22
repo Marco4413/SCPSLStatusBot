fa3ec32de89e469e289b6f101d7a969517b40062
Marco <46133335+Marco4413@users.noreply.github.com>
2022-04-22 16:52:48 +0200

    Added Utils.js

---
+++ Utils.js
@@ -0,0 +1,17 @@
+/**
+ * Formats the specified String with the specified Formats
+ * 
+ * Function stolen from: https://github.com/hds536jhmk/JackBot/blob/latest/Utils.js
+ * @param {String} str The String to Format
+ * @param {...Any} formats The Formats to format the String with
+ * @returns {String} The formatted String
+ */
+const FormatString = (str, ...formats) => {
+    if (formats.length === 0) return str;
+    return str.replace(/{(\d+)}/g, (match, number) => {
+        const index = Number.parseInt(number);
+        return index < formats.length ? formats[index] : match;
+    });
+};
+
+module.exports = { FormatString };
